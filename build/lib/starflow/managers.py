8bccaf6bc7c6a44c7559d0cc1158d2cdb8f9c18d
Jen Johnson <jen@Jens-MacBook-Pro.local>
2017-06-09 10:24:34 -0400

    removes redundant files installed by setup.py

--- build/lib/starflow/managers.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-
-class Manager(object):
-    """
-    Base class for all Manager classes in StarFlow
-    """
-    def __init__(self, cfg):
-        self.cfg = cfg
--- build/lib/starflow/tests/__init__.py
+++
@@ -1,7 +0,0 @@
-import unittest
-import tempfile
-from starflow.config import StarFlowConfig
-from starflow.templates.config import config_template
-
-class StarFlowTest(unittest.TestCase):
-    pass
